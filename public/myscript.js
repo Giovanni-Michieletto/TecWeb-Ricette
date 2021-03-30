27ee9e15e0f9dce36d88bddc5051aa4331bf80e9
SimoneDalMedico <simone.dalmedico@studenti.unipd.it>
2021-03-30 11:50:47 +0200

    torna su

---
+++ public/myscript.js
@@ -0,0 +1,9 @@
+// $('#TornaSu').click(
+//    function(){
+//       $("html, body").animate({ scrollTop: 0 }, "slow");
+//    }
+// )
+
+function Tornasu(){
+   $("html, body").animate({ scrollTop: 0 }, "slow");
+}
